0d2b12c5c801489142685e51611048afe21337ab
TheFreakyKids <robby.patterson95@yahoo.com>
2015-11-20 11:06:16 -0800

    Initial Audio Framework

---
+++ New Unity Project/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioManager : MonoBehaviour 
+{
+	public static AudioManager instance = null;
+	public GameObject audioItem;
+	
+	// Use this for initialization
+	void Awake() 
+	{
+		if (instance == null)
+			instance = this;
+		else if (instance != this)
+			Destroy (gameObject);
+		
+		DontDestroyOnLoad (gameObject);
+	}
+	
+	// Update is called once per frame
+	public void Play(AudioClip clip) 
+	{
+		GameObject go = (GameObject)Instantiate (audioItem);
+		AudioSource src = go.GetComponent<AudioSource> ();
+		src.clip = clip;
+
+		src.Play ();
+		Destroy (go, clip.length);
+	}
+}
