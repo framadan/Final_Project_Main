bc42654cf21c4cdcd95d5c1c30fd4f2ef77636a4
Wyatt Thornbury <w.m.thornbury@hotmail.com>
2015-11-23 04:56:57 -0800

    Few More Comments

--- Coding_Main_Project/Assets/VinceGameManager/GameManager/GameManager.cs
+++ Coding_Main_Project/Assets/VinceGameManager/GameManager/GameManager.cs
@@ -6,6 +6,7 @@ public class GameManager : MonoBehaviour {
     //you have a instance variable, but you never use it to get access to your GameManager. 
     //This is the correct approach and when you need access in other scripts, do a public GameManager gameManager = GameManager.instance; rather then a Find and Get Component.
 	public static GameManager instance = null;
+    //Should be selectedCharacters? seems like players != the character they selected.
 	public GameObject[] players = null;
     //selectedStage seems more appropraite
 	public GameObject selectedScene = null;
