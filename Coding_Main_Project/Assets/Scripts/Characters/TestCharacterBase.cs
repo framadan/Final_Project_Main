1afe2cd3294a1396db4b02df71a1ae357a709db7
solo369 <solo3690@yahoo.com>
2015-11-22 19:59:00 -0800

    vince

--- Coding_Main_Project/Assets/Scripts/Characters/TestCharacterBase.cs
+++ Coding_Main_Project/Assets/Scripts/Characters/TestCharacterBase.cs
@@ -1,34 +1,35 @@
 ﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 using StateMachine;
 using Character;
 
 public class TestCharacterBase : MonoBehaviour
 {
-    CharacterAccessors characterAccessor;
-    CharacterStateMachine stateMachine;
-
-    void Awake()
-    {
-        characterAccessor = gameObject.GetComponent<CharacterAccessors>();
-        
-    }
-
-    
-    void Start ()
-    {
-        stateMachine = characterAccessor.States;
-        stateMachine.AddState(States.test1, ()=> Debug.Log("called Callback Function"));
-        stateMachine.ActiveStates.Add(States.test2);
-        stateMachine.DebugActiveStates();
-        //stateMachine.RemoveState(States.test1);
-
+	CharacterAccessors characterAccessor;
+	CharacterStateMachine stateMachine;
+	
+	void Awake()
+	{
+		characterAccessor = gameObject.GetComponent<CharacterAccessors>();
+		
+	}
+	
+	
+	void Start ()
+	{
+		stateMachine = characterAccessor.States;
+		stateMachine.AddState(States.test1, ()=> Debug.Log("called Callback Function"));
+		stateMachine.ActiveStates.Add(States.test2);
+		stateMachine.DebugActiveStates();
+		//stateMachine.RemoveState(States.test1);
+		
 	}
 	
 	
 	void Update ()
-    {
-        if(Input.GetKeyDown(KeyCode.A))
-            stateMachine.AddState(States.test2);
+	{
+		if(Input.GetKeyDown(KeyCode.A))
+			stateMachine.AddState(States.test2);
 	}
 }
---
+++ Coding_Main_Project/Assets/VinceGameManager/GameManager/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+	public static GameManager instance = null;
+	public GameObject[] players = null;
+	public GameObject selectedScene = null;
+
+	void Awake()
+	{
+		instanceCheck ();
+		DontDestroyOnLoad(transform.gameObject);
+	}
+	void Start () 
+	{
+	
+	}
+	
+
+	void Update () 
+	{
+	
+	}
+	void instanceCheck()
+	{
+		if (instance == null) 
+			instance = this;
+		 else if (instance != this) 
+			Destroy (gameObject);
+	}
+}
