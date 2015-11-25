60d7be16da8102b09d9c7ff474429873fe63b455
SERVER1992 <esiqueiros@student.rcc.edu>
2015-11-25 00:11:41 -0800

    Trying to sync unsynced files that were unable to sync

--- PGscene_AI_scripts_incomplete_atm/AI_Gam24_Final/Assets/AI_Main.cs
+++ PGscene_AI_scripts_incomplete_atm/AI_Gam24_Final/Assets/AI_Main.cs
@@ -1,17 +1,16 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
-
+using System.Linq;
 
 public class AI_Main : MonoBehaviour 
 {
     public float jump = 600f;
-
     public Collider[] possibleTargets;
 	public float speed;
-    public float aggro = 100f;
+    public float aggro = 50f;
 	public GameObject currentTarget;
-
+    
 	public bool showDebug = false;
 	public Color aggroRadiusColor;
 	public int health;
@@ -20,6 +19,7 @@ public class AI_Main : MonoBehaviour
 	public float delayTime;
 	public float delay = 2f;
 	public GameObject ledgegrab;
+    public Transform target;
 	void DrawGismos()
 	{
 		if (showDebug != true)
@@ -34,10 +34,8 @@ public class AI_Main : MonoBehaviour
 	}
 	void Start () 
 	{
-		ledgegrab = GameObject.FindGameObjectWithTag ("Ledge");
+		ledgegrab = GameObject.FindGameObjectWithTag ("Ledge");  
 	}
-
-
     void Update()
     {
         possibleTargets = Physics.OverlapSphere(transform.position, aggro);
@@ -51,6 +49,7 @@ public class AI_Main : MonoBehaviour
             {
                 currentTarget = possibleTarget.gameObject;
             }
+
             if (Vector3.Distance(gameObject.transform.position, currentTarget.transform.position) <= 1f)
             {
                 currentTarget = null;
@@ -68,8 +67,10 @@ public class AI_Main : MonoBehaviour
         if (currentTarget)
         {
             Vector3 direction = (currentTarget.transform.position - transform.position).normalized;
-
-            gameObject.transform.LookAt(currentTarget.transform);
+            //Vector3 currentTarget = new Vector3(target.position.x, this.transform.position.y, target.position.z);
+            //this.transform.LookAt(currentTarget);
+            //gameObject.transform.LookAt(currentTarget.transform);
+            transform.LookAt(new Vector3(currentTarget.transform.position.x, transform.position.y, currentTarget.transform.position.z));
             transform.Translate(Vector3.forward * Time.deltaTime * speed);
             return;
         }
@@ -81,11 +82,13 @@ public class AI_Main : MonoBehaviour
             StartCoroutine(Timer(2));
         }
     }
+        
     IEnumerator Timer(float timer)
     {
         yield return new WaitForSeconds(timer);
         self.layer = LayerMask.NameToLayer("Default");
     }
+
 
     /*if (Vector3.Distance (gameObject.transform.position, ledgegrab.transform.position) <= 5.0f) 
     {
@@ -94,26 +97,21 @@ public class AI_Main : MonoBehaviour
 
         transform.Translate(Vector3.forward * Time.deltaTime * speed);
     }*/
-    /*
-            }
-            if (Vector3.Distance (gameObject.transform.position, currentTarget.transform.position) <= 8.0f)
-            {
-                Animation.Play("HeavyPunch");
-            }
-            if (Vector3.Distance (gameObject.transform.position, currentTarget.transform.position) <= 12.0f) 
-            {
-                Animation.Play("RangedAttack");
-            }
-            if (Vector3.Distance (gameObject.transform.position, currentTarget.transform.position) <= 5.0f) 
-            {
-                Animation.Play("Block");
-            }	*/
 
     public void KnockBack(float value)
 	{
 		Vector3 direction = transform.InverseTransformDirection (0,1,1);
 		this.GetComponent<Rigidbody>().AddForce(direction * value);
 	}
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Ledge")
+        {
+            self.transform.LookAt(other.gameObject.transform);
+            gameObject.GetComponent<Rigidbody>().AddForce(Vector3.forward * (100));
+            gameObject.GetComponent<Rigidbody>().AddForce(Vector3.up * jump);
+        }
+    }
 }
 
 
