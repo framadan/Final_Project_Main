858a784a1fa3f00d88cfdf6a5ab5ab728bc10241
PraetorMagnus908 <Praetor908@gmail.com>
2015-11-24 16:12:43 -0800

    Started the coding for Walljump

---
+++ Coding_Main_Project/Assets/Scripts/Characters/Movement/WallJump.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WallJump : MonoBehaviour {
+	
+
+	void WallJumpCheck()
+	{
+
+		if(!isGrounded && ControllerColliderHit.tag == "Wall" && Input.GetKeyDown ("Jump"))
+		{
+			WallJump()
+		}
+		else(!isGrounded && Input.GetKeyDown ("Jump"))
+		{
+			Debug.Log("You're in the air!")
+		}
+	}
+
+	void WallJump()
+	{
+
+
+	}
+
+}
