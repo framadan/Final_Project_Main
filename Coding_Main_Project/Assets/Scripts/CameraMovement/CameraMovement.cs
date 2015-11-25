a108a220cfa1c11931e7b7ece1ba1db0b87b0722
SERVER1992 <esiqueiros@student.rcc.edu>
2015-11-25 14:12:43 -0800

    Added Basic Camera Movement
    It works but still needs lots of implementation.

---
+++ Coding_Main_Project/Assets/Scripts/CameraMovement/CameraMovement.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CameraMovement : MonoBehaviour
+{
+
+    public List<Transform> playerTransforms = new List<Transform>();
+    public Transform player;
+    public Transform randomObject;
+    public float lerpSpeed = 0.0f;
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	
+	void LateUpdate ()
+    {
+        transform.position = Vector3.Lerp (transform.position,new Vector3(player.transform.position.x, player.transform.position.y, transform.position.z),Time.deltaTime * lerpSpeed);
+        transform.position = Vector3.Lerp(transform.position, new Vector3(randomObject.transform.position.x, randomObject.transform.position.y, transform.position.z), Time.deltaTime * lerpSpeed);
+    }
+}
