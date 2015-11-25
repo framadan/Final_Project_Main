d7b72c5168e8fc88739132838e2c0e4dcf0681c1
bubbabrent <elwais12@gmail.com>
2015-11-25 10:48:44 -0800

    Menu and prototypes for levels
    hope this shit works

---
+++ Art_Lead/Assets/Levels&Menu/Menu/menuSwitching.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class menuSwitching : MonoBehaviour 
+{
+	public Canvas start;
+	public Canvas charSelect;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		start.enabled = true;
+		charSelect.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+
+		if (start.enabled == true)
+		{
+			if(Input.GetMouseButton(0))
+			{
+				start.enabled = false;
+				charSelect.enabled = true;
+			}
+		}
+
+		if (charSelect.enabled == true) 
+		{
+			if(Input.GetKey (KeyCode.Backspace))
+			{
+				charSelect.enabled = false;
+				start.enabled = true;
+			}
+		}
+	}
+}
