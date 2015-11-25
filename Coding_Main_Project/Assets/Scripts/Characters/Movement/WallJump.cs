e1766852ee80c214de1c6ed572728d9d554d5edd
SERVER1992 <esiqueiros@student.rcc.edu>
2015-11-25 03:22:39 -0800

    Added Player Ledge Grabbing
    The functionality of ledge grabbing is there, but the code is pretty
    unorganized and not fully completed to work with player controllers.

--- Coding_Main_Project/Assets/Scripts/Characters/Movement/WallJump.cs
+++ Coding_Main_Project/Assets/Scripts/Characters/Movement/WallJump.cs
@@ -1,26 +1,30 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class WallJump : MonoBehaviour {
+public class WallJump : MonoBehaviour
+{
 	
-
-	void WallJumpCheck()
+    
+	/// <summary>
+    /// Backslashed everything due to erros.- Eric S.
+    /// </summary>
+    void WallJumpCheck()
 	{
 
-		if(!isGrounded && ControllerColliderHit.tag == "Wall" && Input.GetKeyDown ("Jump"))
-		{
-			WallJump()
-		}
-		else(!isGrounded && Input.GetKeyDown ("Jump"))
-		{
-			Debug.Log("You're in the air!")
-		}
+		//if(!isGrounded && ControllerColliderHit.tag == "Wall" && Input.GetKeyDown ("Jump"))
+		//{
+        //    WallJump();
+		//}
+		//else(!isGrounded && Input.GetKeyDown ("Jump"))
+		//{
+       //     Debug.Log("You're in the air!");
+		//}
 	}
 
-	void WallJump()
-	{
+	//void WallJump()
+	//{
 
 
-	}
+	//}
 
 }
