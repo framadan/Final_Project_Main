ba77583a93771af51f31282b2dfc7e6993dc4a66
PraetorMagnus908 <Praetor908@gmail.com>
2015-11-27 09:48:09 -0800

    More Work on the WallJump Script still in progress

--- Coding_Main_Project/Assets/Scripts/Characters/Movement/WallJump.cs
+++ Coding_Main_Project/Assets/Scripts/Characters/Movement/WallJump.cs
@@ -2,24 +2,49 @@
 using System.Collections;
 
 public class WallJump : MonoBehaviour {
-	
+	public float Speed = 3f;
+	public float GravityStrength = 5f;
+	public float JumpSpeed = 10f;
+	float verticalVelocity;
+	Vector3 velocity;
+	Vector3 groundedVelocity;
+	bool WallJumping;
 
-	//void WallJumpCheck()
-	//{
+	void Update()
+	{
+		Vector3 myVector = new Vector3(0,0,0);
 
-	//	if(!isGrounded && ControllerColliderHit.tag == "Wall" && Input.GetKeyDown ("Jump"))
-	//	{
-	//		WallJump()
-	//	}
-	//	else(!isGrounded && Input.GetKeyDown ("Jump"))
-	//	{
-	//		Debug.Log("You're in the air!")
-	//	}
-	//}
+		if (CharacterController.isGrounded)
+		{
+			myVector.x = Imput.GetAxis("HOrizontal");
+			myVector.y = Input.GetAxis("Vertical");
+			myVector = Vector3.ClampMagnitude(myVector, 1f);
+			myVector = myVector * Speed * Time.deltaTime;
 
-	//void WallJump()
-	//{
+		}
+		else
+		{
+			myVector = groundedVelocity;
+			myVector*= Time.deltaTime;
+		}
 
 
-	//}
+	}
+	void WallJumpCheck()
+	{
+
+		if(!isGrounded && ControllerColliderHit.tag == "Wall" && Input.GetKeyDown ("Jump"))
+		{
+			WallJumping()
+		}
+		else(!isGrounded && Input.GetKeyDown ("Jump"))
+		{
+			Debug.Log("You're in the air!")
+		}
+	}
+	void OnControllerColliderHit(ConrollerColliderHit hit)
+	{
+		Vector3 normal = hit.normal;
+		Vector3.Reflect()
+	}
 }
