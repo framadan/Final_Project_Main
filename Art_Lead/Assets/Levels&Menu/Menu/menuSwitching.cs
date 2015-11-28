754f04f41302c8748fe0b97a556b25d86703d926
bubbabrent <elwais12@gmail.com>
2015-11-27 18:40:50 -0800

    stage selection buttons 2/3
    battlefield and final destination as of right now, for buttons, as well
    as functionality of going back to the starting screen.

--- Art_Lead/Assets/Levels&Menu/Menu/menuSwitching.cs
+++ Art_Lead/Assets/Levels&Menu/Menu/menuSwitching.cs
@@ -6,12 +6,14 @@ public class menuSwitching : MonoBehaviour
 {
 	public Canvas start;
 	public Canvas charSelect;
+    public Canvas stage;
 	
 	// Use this for initialization
 	void Start ()
 	{
 		start.enabled = true;
 		charSelect.enabled = false;
+        stage.enabled = false;
 	}
 	
 	// Update is called once per frame
@@ -35,6 +37,19 @@ public class menuSwitching : MonoBehaviour
 				charSelect.enabled = false;
 				start.enabled = true;
 			}
+            if (Input.GetKey(KeyCode.Return))
+            {
+                charSelect.enabled = false;
+                stage.enabled = true;
+            }
 		}
+        if (stage.enabled == true)
+        {
+            if (Input.GetKey(KeyCode.Backspace))
+            {
+                stage.enabled = false;
+                charSelect.enabled = true;
+            }
+        }
 	}
 }
