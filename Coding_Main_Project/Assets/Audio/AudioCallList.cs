2e5297c03e42e7238c9e20b97677f1b9e7dd287d
Aaron Eastwood <aaroneastwood@aaroneastwood.com>
2015-11-27 22:20:55 -0800

    Audio materials moved to Coding_Main_Project

---
+++ Coding_Main_Project/Assets/Audio/AudioCallList.cs
@@ -0,0 +1,12 @@
+﻿/*using UnityEngine;
+using System.Collections;
+
+public class AudioCallList : MonoBehaviour 
+{
+	public AudioClip clip = null;
+
+//	AudioManager.instance.Play(clip);
+
+
+}
+*/
