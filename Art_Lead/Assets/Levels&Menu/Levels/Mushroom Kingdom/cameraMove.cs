485f3c9531b8d8d7acae07c3b9735faa4061b59d
bubbabrent <elwais12@gmail.com>
2015-11-28 17:12:58 -0800

    Level and button things
    mushroom kingdom prefabs created, as well as camera movement to go right
    and then left. button selections for 5 levels (final destination,
    battlefield, mushroom kingdom, pokemon floats, onett)

--- Art_Lead/Assets/Levels&Menu/Levels/Mushroom Kingdom/cameraMove.cs
+++ Art_Lead/Assets/Levels&Menu/Levels/Mushroom Kingdom/cameraMove.cs
@@ -3,7 +3,8 @@ using System.Collections;
 
 public class cameraMove : MonoBehaviour 
 {
-	public float speed = 10.0f;
+	public float speed = 20.0f;
+    public float timer = 0.0f;
 	// Use this for initialization
 	void Start ()
 	{
@@ -13,6 +14,22 @@ public class cameraMove : MonoBehaviour
 	// Update is called once per frame
 	void Update () 
 	{
+        timer = timer + Time.deltaTime;
+        if (timer < 100)
+        {
+            speed = 20;
+        }
+        if (timer > 100)
+        {
+            speed = -20;
+        }
+        if (timer >= 200)
+        {
+            timer = 0;
+        }
 		gameObject.transform.Translate (Vector3.right * speed * Time.deltaTime);
+        //print(speed);
+       
 	}
+   
 }
