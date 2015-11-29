94b5edec20099511b9ec1f98bd7a7e50f12b3c6e
SERVER1992 <esiqueiros@student.rcc.edu>
2015-11-29 02:24:29 -0800

    Improved the CameraMovement Script
    Changed the camera movement scripts completely. Feel free to check on
    the script by opening the PlayerMovement scene.

---
+++ Coding_Main_Project/Assets/Scripts/CameraMovement/CameraDistanceDetecter.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraDistanceDetecter : MonoBehaviour
+{
+
+    public GameObject playerOne;
+    public GameObject playerTwo;
+    public GameObject playerThree;
+    public GameObject playerFour;
+
+    public float playerOneDistance;
+    public float playerTwoDistance;
+    public float playerThreeDistance;
+    public float playerFourDistance;
+
+	// Use this for initialization
+	void Start ()
+    {
+        PlayerFinder();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        PlayerDistanceFinder();
+    }
+
+    public void PlayerFinder()
+    {
+        playerOne = GameObject.FindWithTag("PlayerOne");
+        playerTwo = GameObject.FindWithTag("PlayerTwo");
+        playerThree = GameObject.FindWithTag("PlayerThree");
+        playerFour = GameObject.FindWithTag("PlayerFour");
+    }
+
+    public void PlayerDistanceFinder()
+    {
+        playerOneDistance = Vector3.Distance(playerOne.transform.position, transform.position);
+        playerTwoDistance = Vector3.Distance(playerTwo.transform.position, transform.position);
+        playerThreeDistance = Vector3.Distance(playerThree.transform.position, transform.position);
+        playerFourDistance = Vector3.Distance(playerFour.transform.position, transform.position);
+    }
+}
--- Coding_Main_Project/Assets/Scripts/CameraMovement/CameraMovement.cs
+++ Coding_Main_Project/Assets/Scripts/CameraMovement/CameraMovement.cs
@@ -5,21 +5,63 @@ using System.Collections.Generic;
 public class CameraMovement : MonoBehaviour
 {
 
-    public List<Transform> playerTransforms = new List<Transform>();
-    public Transform player;
-    public Transform randomObject;
+    //public List<Transform> playerTransforms = new List<Transform>();
+
+    //public Transform player;
+
+    //Note: Some of these variables need to be set to private. Only set to public as of right now just to see
+    //      the values in the inspector. - Eric S. 11/29/2015
+    public float cameraPositionZ;
+    public float cameraPositionZOffset = 0.0f;
+    public float cameraPositionZLimit = 0.0f;
+
+    public float playerOnePositionZ;
+    public float playerTwoPositionZ;
+    public float playerThreePositionZ;
+    public float playerFourPositionZ;
     public float lerpSpeed = 0.0f;
+
 
 	// Use this for initialization
 	void Start ()
     {
-	
-	}
-	
-	
+        
+    }
+
+    void Update()
+    {
+        PlayerPositionDetection();
+        CameraPositionDetection();
+    }
+
 	void LateUpdate ()
     {
-        transform.position = Vector3.Lerp (transform.position,new Vector3(player.transform.position.x, player.transform.position.y, transform.position.z),Time.deltaTime * lerpSpeed);
-        transform.position = Vector3.Lerp(transform.position, new Vector3(randomObject.transform.position.x, randomObject.transform.position.y, transform.position.z), Time.deltaTime * lerpSpeed);
+        CameraMovementFunction();
+    }
+
+    public void PlayerPositionDetection()
+    {
+        GameObject camDistanceDetector = GameObject.Find("CamDistanceDetector");
+        CameraDistanceDetecter camDistanceDetectorScript = camDistanceDetector.GetComponent<CameraDistanceDetecter>();
+        playerOnePositionZ = camDistanceDetectorScript.playerOneDistance;
+        playerTwoPositionZ = camDistanceDetectorScript.playerTwoDistance;
+        playerThreePositionZ = camDistanceDetectorScript.playerThreeDistance;
+        playerFourPositionZ = camDistanceDetectorScript.playerFourDistance;
+    }
+
+    public void CameraPositionDetection()
+    {
+        cameraPositionZ = -playerOnePositionZ + -playerTwoPositionZ + -playerThreePositionZ + -playerFourPositionZ + -cameraPositionZOffset;
+        if (cameraPositionZ <= cameraPositionZLimit)
+        {
+            cameraPositionZ = cameraPositionZLimit;
+        }
+    }
+
+    public void CameraMovementFunction()
+    {
+        //transform.position = Vector3.Lerp (transform.position,new Vector3(player.transform.position.x, player.transform.position.y, transform.position.z),Time.deltaTime * lerpSpeed);
+        //transform.position = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+        transform.position = Vector3.Lerp(transform.position, new Vector3(transform.position.x, transform.position.y, cameraPositionZ), Time.deltaTime * lerpSpeed);
     }
 }
