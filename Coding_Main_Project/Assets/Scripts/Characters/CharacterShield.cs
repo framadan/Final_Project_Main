ca9f76c6c7732fa0020239c582a7328c4b9f7e86
SERVER1992 <esiqueiros@student.rcc.edu>
2015-11-29 14:18:20 -0800

    Added Player Shield
    Added Player Shield

---
+++ Coding_Main_Project/Assets/Scripts/Characters/CharacterShield.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterShield : MonoBehaviour
+{
+
+    public bool shieldActive = false;
+    public int health = 0;
+    public float timeForDamage = 0.0f;
+    public float timeForActive = 0.0f;
+    
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        ShieldActivity();
+        ShieldState();
+	}
+
+    public void ShieldActivity()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            if (timeForActive <= 0.0f)
+            {
+                shieldActive = true;
+            }
+        }
+        else
+            shieldActive = false;
+    }
+
+    public void ShieldState()
+    {
+        if (shieldActive == true)
+        {
+            timeForDamage -= Time.deltaTime;
+
+            if (timeForDamage <= 0.0f)
+            {
+                health -= 1;
+                timeForDamage = 5.0f;
+                shieldActive = false;
+                timeForActive = 3.0f;
+            }
+        }
+        else
+            timeForDamage = 5.0f;
+        if (timeForActive >= 3.0f || timeForActive<= 3.0f)
+        {
+            timeForActive = timeForActive - Time.deltaTime;
+            if (timeForActive <= 0.0f)
+            {
+                timeForActive = 0.0f;
+            }
+        }
+    }
+}
--- Coding_Main_Project/Assets/Scripts/Characters/Movement/WallJump.cs
+++ Coding_Main_Project/Assets/Scripts/Characters/Movement/WallJump.cs
@@ -1,7 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class WallJump : MonoBehaviour {
+public class WallJump : MonoBehaviour
+{
 	public float Speed = 3f;
 	public float GravityStrength = 5f;
 	public float JumpSpeed = 10f;
@@ -11,42 +12,42 @@ public class WallJump : MonoBehaviour {
 	Vector3 normal;
 	bool WallJumping;
 
-	void Update()
-	{
-		Vector3 myVector = new Vector3(0,0,0);
+	//void Update()
+	//{
+		//Vector3 myVector = new Vector3(0,0,0);
 
-		if (CharacterController.isGrounded)
-		{
-			myVector.x = Imput.GetAxis("HOrizontal");
-			myVector.y = Input.GetAxis("Vertical");
-			myVector = Vector3.ClampMagnitude(myVector, 1f);
-			myVector = myVector * Speed * Time.deltaTime;
+		//if (CharacterController.isGrounded)
+		//{
+		//	myVector.x = Imput.GetAxis("HOrizontal");
+		//	myVector.y = Input.GetAxis("Vertical");
+		//	myVector = Vector3.ClampMagnitude(myVector, 1f);
+		//	myVector = myVector * Speed * Time.deltaTime;
 
-		}
-		else
-		{
-			myVector = groundedVelocity;
-			myVector*= Time.deltaTime;
-		}
-		myVector.y = verticalVelocity*Time.deltaTime;
+		//}
+		//else
+		//{
+		//	myVector = groundedVelocity;
+		//	myVector*= Time.deltaTime;
+		//}
+		//myVector.y = verticalVelocity*Time.deltaTime;
 
-	}
-	void WallJumpCheck()
-	{
+	//}
+	//void WallJumpCheck()
+	//{
 
-		if(!isGrounded && ControllerColliderHit.tag == "Wall" && Input.GetKeyDown ("Jump"))
-		{
-			groundVelocity = Vector3.Reflect(groundedVelocity, normal);
-			WallJumping()
-		}
-		else(!isGrounded && Input.GetKeyDown ("Jump"))
-		{
-			Debug.Log("You're in the air!")
-		}
-	}
-	void OnControllerColliderHit(ControllerColliderHit hit)
-	{
-		normal = hit.normal;
+		//if(!isGrounded && ControllerColliderHit.tag == "Wall" && Input.GetKeyDown ("Jump"))
+		//{
+			//groundVelocity = Vector3.Reflect(groundedVelocity, normal);
+			//WallJumping()
+		//}
+		//else(!isGrounded && Input.GetKeyDown ("Jump"))
+		//{
+		//	Debug.Log("You're in the air!")
+		//}
+	//}
+	//void OnControllerColliderHit(ControllerColliderHit hit)
+	//{
+	//	normal = hit.normal;
 
-	}
+	//}
 }
