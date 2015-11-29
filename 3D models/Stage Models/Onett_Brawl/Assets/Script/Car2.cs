6e348baa8f18f1ec03b98b179663cc5748f0b8c1
Doom13 <jfgonzalezc94@gmail.com>
2015-11-29 15:38:02 -0800

    Onett Stage
    The stage is pretty much game ready. Only needs the cars to do damage to
    players. Other than that the cars spawn, they move and there is a
    trigger that warns players of an oncoming car

---
+++ 3D models/Stage Models/Onett_Brawl/Assets/Script/Car2.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Car2 : MonoBehaviour 
+{
+	public float speed = 2.0f;
+	
+	
+	// Use this for initialization
+	void Start () 
+	{
+	}
+	
+	// Update is called once per frame	
+	void Update () 
+	{
+		transform.Translate (Vector3.left * Time.deltaTime * speed);
+	}
+	void OnCollisionEnter (Collision other)
+	{
+	}
+}
+
