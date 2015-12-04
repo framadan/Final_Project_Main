3d98e0839e9a01c20340b0fc3e54246b384d3553
pcataldo <pcataldo@student.rcc.edu>
2015-12-04 10:22:56 -0800

    FireFlower package + RayGun and MaxTomato scripts

---
+++ New Unity Project/Assets/JB Items/Source/Item Scripts/MaxTomato.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MaxTomato : MonoBehaviour 
+{
+	public float healAmount = 0.0f;
+
+	public AudioClip eatFood = null;
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+
+	public void GetHealed(ItemUser weilder)
+	{
+		weilder.GetBuffed (null, eatFood, 0, healAmount, 1);
+	}
+
+}
