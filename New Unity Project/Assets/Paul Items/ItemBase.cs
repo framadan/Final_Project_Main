856a54da19101bb82a0f6dcfb2d5825f8bb93c75
pcataldo <pcataldo@student.rcc.edu>
2015-12-04 11:30:43 -0800

    ItemsReborn For real this time

---
+++ New Unity Project/Assets/Paul Items/ItemBase.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ItemBase : MonoBehaviour 
+{
+
+	public virtual void Consume ()
+	{
+		print ("base class");
+	}
+	
+	public void Use ()
+	{
+		
+	}
+}
