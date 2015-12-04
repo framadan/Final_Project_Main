53f5e48e8cb7a26ffd5bbf289a4d92644f8c92ec
pcataldo <pcataldo@student.rcc.edu>
2015-12-04 15:39:26 -0800

    StarRod Enters the Fight
    Also: MaxTomato renamed to FoodItem. Can be applied to any healing item
    and adjusted in unity. MetalBox needs to be updated depending on
    character controls.

---
+++ Coding_Main_Project/Assets/Paul Items/Completed Item Scripts/FoodItem.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FoodItem : ItemBase 
+{
+	public float healAmount = 0.0f; //Adjustable for different foods
+
+	public AudioClip eatFood = null;
+
+
+	public override void Consume()
+	{
+		//play audioclip
+		//Restore health
+	}
+
+}
---
+++ Coding_Main_Project/Assets/Paul Items/Completed Item Scripts/StarRod.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StarRod : ItemBase 
+{
+
+	public float damage = 17.0f;
+	public float baseKnockBack = 22.0f;
+
+	public int ammo = 18;
+	private int ammoCost = 1;
+
+	public GameObject StarShot = null;
+	
+	public override void SmashUse()
+	{
+		damage = 50.0f;
+		baseKnockBack = 47.0f;
+
+		if (ammo > 0) 
+		{
+			Instantiate (StarShot, transform.position, transform.rotation);
+			ammo -= ammoCost;
+		}
+		
+	}
+	public override void Use()
+	{
+		damage = 17.0f;
+		baseKnockBack = 22.0f;
+	}
+}
