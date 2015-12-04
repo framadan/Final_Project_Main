78f08c0fa9703a48f7b810c526448a5eec8af040
PraetorMagnus908 <Praetor908@gmail.com>
2015-12-04 13:48:29 -0800

    Added weapon scripts with set damage and baseKnockBack overrides

---
+++ Coding_Main_Project/Assets/Paul Items/Hammer.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hammer : ItemBase
+{
+	public float damage = 125.0f;
+	public float baseKnockBack = 90.0f;
+
+
+	override void Use()
+	{
+		damage = 125.0f;
+		baseKnockBack = 90.0f;
+
+
+	}
+}
--- Coding_Main_Project/Assets/Paul Items/HomeRunBat.cs
+++ Coding_Main_Project/Assets/Paul Items/HomeRunBat.cs
@@ -4,15 +4,17 @@ using System.Collections;
 public class HomeRunBat : ItemBase 
 {
 
-	// Use this for initialization
-	void Start () 
+	public float baseKnockBack = 20.0f;
+	public float damage = 15.0f;
+
+	override void SmashUse() 
 	{
-		
+		baseKnockBack = 9000.0f;
+		damage = 9000.0f;
 	}
-	
-	// Update is called once per frame
-	void Update () 
+	override void Use()
 	{
-		
+		baseKnockBack = 20.0f;
+		damage = 15.0f;
 	}
 }
