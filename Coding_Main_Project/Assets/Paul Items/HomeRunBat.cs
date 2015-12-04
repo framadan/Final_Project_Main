f5b0401a6a3e53f5cccc5ca974586969ef439241
PraetorMagnus908 <Praetor908@gmail.com>
2015-12-04 11:47:23 -0800

    shtuff

---
+++ Coding_Main_Project/Assets/Paul Items/HomeRunBat.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HomeRunBat : ItemBase 
+{
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+}
