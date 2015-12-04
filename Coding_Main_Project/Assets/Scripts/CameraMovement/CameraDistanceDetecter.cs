5976571b37cf7c8fa315869c662f634f359aac71
SERVER1992 <esiqueiros@student.rcc.edu>
2015-12-04 15:30:58 -0800

    Changes to Camera Movement
    Changed Camera Movement
    Note: Not Finished

--- Coding_Main_Project/Assets/Scripts/CameraMovement/CameraDistanceDetecter.cs
+++ Coding_Main_Project/Assets/Scripts/CameraMovement/CameraDistanceDetecter.cs
@@ -1,18 +1,24 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Linq;
+using System.Collections.Generic;
 
 public class CameraDistanceDetecter : MonoBehaviour
 {
+	public List<GameObject> playerObjects;
+	public PlayerMovement[] movementArray = null;
+	public float playerObjectsDistance;
+	public CameraMovement cameraMovement = null;
 
-    public GameObject playerOne;
-    public GameObject playerTwo;
-    public GameObject playerThree;
-    public GameObject playerFour;
+//    public GameObject playerOne;
+//    public GameObject playerTwo;
+//    public GameObject playerThree;
+//    public GameObject playerFour;
 
-    public float playerOneDistance;
-    public float playerTwoDistance;
-    public float playerThreeDistance;
-    public float playerFourDistance;
+//    public float playerOneDistance;
+//    public float playerTwoDistance;
+//    public float playerThreeDistance;
+//    public float playerFourDistance;
 
 	// Use this for initialization
 	void Start ()
@@ -28,17 +34,43 @@ public class CameraDistanceDetecter : MonoBehaviour
 
     public void PlayerFinder()
     {
-        playerOne = GameObject.FindWithTag("PlayerOne");
-        playerTwo = GameObject.FindWithTag("PlayerTwo");
-        playerThree = GameObject.FindWithTag("PlayerThree");
-        playerFour = GameObject.FindWithTag("PlayerFour");
+		movementArray =  (PlayerMovement[])GameObject.FindObjectsOfType (typeof(PlayerMovement));
+		foreach (PlayerMovement temp in movementArray) 
+		{
+			playerObjects.Add(temp.gameObject);
+		}
+
+//        playerOne = GameObject.FindWithTag("PlayerOne");
+//        playerTwo = GameObject.FindWithTag("PlayerTwo");
+//        playerThree = GameObject.FindWithTag("PlayerThree");
+//        playerFour = GameObject.FindWithTag("PlayerFour");
     }
 
     public void PlayerDistanceFinder()
     {
-        playerOneDistance = Vector3.Distance(playerOne.transform.position, transform.position);
-        playerTwoDistance = Vector3.Distance(playerTwo.transform.position, transform.position);
-        playerThreeDistance = Vector3.Distance(playerThree.transform.position, transform.position);
-        playerFourDistance = Vector3.Distance(playerFour.transform.position, transform.position);
+		float xMin = 0;
+		float xMax = 0;
+		for (int i =0; i < playerObjects.Count; i ++) 
+		{
+			if (i == 0) {
+				xMin = playerObjects [i].transform.position.x;
+				xMax = playerObjects [i].transform.position.x;
+				//cameraMovement.yMax = playerObjects[i].transform.position.x;
+				continue;
+			}
+
+			if (playerObjects [i].transform.position.x < xMin) {
+				xMin = playerObjects [i].transform.position.x;
+			} else if (playerObjects [i].transform.position.x > xMax) {
+				xMax = playerObjects [i].transform.position.x;
+			}
+		}
+		cameraMovement.xMax = xMax;
+		cameraMovement.xMin = xMin;
+
+        //playerOneDistance = Vector3.Distance(playerOne.transform.position, transform.position);
+       // playerTwoDistance = Vector3.Distance(playerTwo.transform.position, transform.position);
+       // playerThreeDistance = Vector3.Distance(playerThree.transform.position, transform.position);
+        //playerFourDistance = Vector3.Distance(playerFour.transform.position, transform.position);
     }
 }
--- Coding_Main_Project/Assets/Scripts/CameraMovement/CameraMovement.cs
+++ Coding_Main_Project/Assets/Scripts/CameraMovement/CameraMovement.cs
@@ -1,13 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 
 public class CameraMovement : MonoBehaviour
 {
 
     //public List<Transform> playerTransforms = new List<Transform>();
+	public float xMin = 0, xMax = 0, yMax = 0;
+	public List<float> yPosition = new List<float> ();
+	public List<GameObject> players = new List<GameObject> ();
+	public GameObject player;
 
-    //public Transform player;
 
     //Note: Some of these variables need to be set to private. Only set to public as of right now just to see
     //      the values in the inspector. - Eric S. 11/29/2015
@@ -15,17 +19,19 @@ public class CameraMovement : MonoBehaviour
     public float cameraPositionZOffset = 0.0f;
     public float cameraPositionZLimit = 0.0f;
 
-    public float playerOnePositionZ;
-    public float playerTwoPositionZ;
-    public float playerThreePositionZ;
-    public float playerFourPositionZ;
+	public float DistanceForPlayers;
+//    public float playerOnePositionZ;
+//    public float playerTwoPositionZ;
+//    public float playerThreePositionZ;
+//    public float playerFourPositionZ;s
     public float lerpSpeed = 0.0f;
 
 
 	// Use this for initialization
 	void Start ()
     {
-        
+		players = GameObject.FindGameObjectsWithTag ("Player").ToList();
+
     }
 
     void Update()
@@ -43,19 +49,27 @@ public class CameraMovement : MonoBehaviour
     {
         GameObject camDistanceDetector = GameObject.Find("CamDistanceDetector");
         CameraDistanceDetecter camDistanceDetectorScript = camDistanceDetector.GetComponent<CameraDistanceDetecter>();
-        playerOnePositionZ = camDistanceDetectorScript.playerOneDistance;
-        playerTwoPositionZ = camDistanceDetectorScript.playerTwoDistance;
-        playerThreePositionZ = camDistanceDetectorScript.playerThreeDistance;
-        playerFourPositionZ = camDistanceDetectorScript.playerFourDistance;
+		DistanceForPlayers = camDistanceDetectorScript.playerObjectsDistance;
+
+//        playerOnePositionZ = camDistanceDetectorScript.playerOneDistance;
+//        playerTwoPositionZ = camDistanceDetectorScript.playerTwoDistance;
+//        playerThreePositionZ = camDistanceDetectorScript.playerThreeDistance;
+//        playerFourPositionZ = camDistanceDetectorScript.playerFourDistance;
     }
 
     public void CameraPositionDetection()
     {
-        cameraPositionZ = -playerOnePositionZ + -playerTwoPositionZ + -playerThreePositionZ + -playerFourPositionZ + -cameraPositionZOffset;
-        if (cameraPositionZ <= cameraPositionZLimit)
-        {
-            cameraPositionZ = cameraPositionZLimit;
-        }
+//		cameraPositionZ = -playerOnePositionZ + -playerTwoPositionZ + -playerThreePositionZ + -playerFourPositionZ + -cameraPositionZOffset;
+//        if (cameraPositionZ <= cameraPositionZLimit)
+//        {
+//            cameraPositionZ = cameraPositionZLimit;
+//        }
+
+		cameraPositionZ =  -DistanceForPlayers + -cameraPositionZOffset;
+		if (cameraPositionZ <= cameraPositionZLimit)
+		{
+			cameraPositionZ = cameraPositionZLimit;
+		}
     }
 
     public void CameraMovementFunction()
