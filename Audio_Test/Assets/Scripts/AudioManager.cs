f919ff53bb7919b194e12375097965fbe7a846d7
Aaron Eastwood <aaroneastwood@aaroneastwood.com>
2015-12-08 16:54:11 -0800

    Audio manager updated. BGM and hit SFX now play in audio test scene. Created package containing basic audio elements.

---
+++ Audio_Test/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+using System.Collections;
+
+public class AudioManager : MonoBehaviour 
+{
+	public static AudioManager instance = null;
+	public GameObject audioItem;
+
+	public AudioMixerGroup voiceMixer = null;
+	public AudioMixerGroup sfxMixer = null;
+	public AudioMixerGroup musicMixer = null;
+	
+	// Use this for initialization
+	void Awake() 
+	{
+		if (instance == null)
+			instance = this;
+		else if (instance != this)
+			Destroy (gameObject);
+		
+		DontDestroyOnLoad (gameObject);
+	}
+	
+
+	// Update is called once per frame
+	public void PlayVoice(AudioClip clip) 
+	{
+		GameObject go = Instantiate (audioItem);
+		AudioSource src = go.GetComponent<AudioSource> ();
+		src.outputAudioMixerGroup = voiceMixer;
+		src.clip = clip;
+		src.Play ();
+		Destroy (go, clip.length);
+	}
+
+	// Update is called once per frame
+	public void PlaySound(AudioClip clip) 
+	{
+		GameObject go = Instantiate (audioItem);
+		AudioSource src = go.GetComponent<AudioSource> ();
+		src.outputAudioMixerGroup = sfxMixer;
+		src.clip = clip;
+		src.pitch = Random.Range (0.95f, 1.05f);
+		src.Play ();
+		Destroy (go, clip.length);
+	}
+
+	public void PlayMusic(AudioClip clip) 
+	{
+		GameObject go = Instantiate (audioItem);
+		AudioSource src = go.GetComponent<AudioSource> ();
+		src.outputAudioMixerGroup = musicMixer;
+		src.clip = clip;
+		src.loop = true;
+		src.Play ();
+		//Destroy (go, clip.length);
+	}
+}
---
+++ Audio_Test/Assets/Scripts/GameManagerStandIn.cs
@@ -0,0 +1,33 @@
+﻿//The game object prefab called "AudioManager" should be placed into the scene or instantiated on Awake
+
+using UnityEngine;
+using System.Collections;
+
+public class GameManagerStandIn : MonoBehaviour 
+
+{
+	public GameObject audioManager = null;
+
+	//Audio Clips should be declared in the script from which their respective play functions are called
+	public AudioClip stageMusic = null;
+	public AudioClip normalHit = null;
+	public AudioClip critHit = null;
+
+	void Awake () 
+	{
+		//This instantiates the audioManager prefab on Awake so that the music and sound effects can play. It must be in the scene before any audio will play.
+		Instantiate (audioManager);
+		//This calls the fuction contained in the AudioManager script that plays music. The stageMusic clip should be passed as an argument.
+		AudioManager.instance.PlayMusic (stageMusic);
+	}
+
+	void Update ()
+	{ 
+		if (Input.GetKeyDown ("p"))
+			//This calls the function contained in the AudioManager script that plays a sound effect. The approprate audio clip should be passed as an argument.
+			AudioManager.instance.PlaySound (normalHit);
+
+		if (Input.GetKeyDown ("c"))
+			AudioManager.instance.PlaySound (critHit);
+	}
+}
