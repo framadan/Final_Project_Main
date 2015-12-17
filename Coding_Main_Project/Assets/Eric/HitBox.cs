59cd3303195e294adeb9aeb893c274e4aa802855
SERVER1992 <esiqueiros@student.rcc.edu>
2015-12-16 20:34:59 -0800

    Added Simple Hit Box
    Simple Hit Box is made. Needs to be modified.

---
+++ Coding_Main_Project/Assets/Eric/HitBox.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HitBox : MonoBehaviour
+{
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            other.rigidbody.AddForce(transform.right * 600);
+        }
+    }
+}
