e3bbee9c1420bdcc35d02b8110eb7b4a140f283d
SERVER1992 <esiqueiros@student.rcc.edu>
2015-12-16 20:57:59 -0800

    Added Hit Box Timer
    Hit Box now gets destroyed after being instantiated.

--- Coding_Main_Project/Assets/Eric/HitBox.cs
+++ Coding_Main_Project/Assets/Eric/HitBox.cs
@@ -4,11 +4,12 @@ using System.Collections;
 public class HitBox : MonoBehaviour
 {
     public float force = 600.0f;
+    public float timeSpentActive = 0.3f;
 
     // Update is called once per frame
     void Update()
     {
-
+        TriggerDestroy();
     }
 
     public void OnCollisionEnter(Collision other)
@@ -17,6 +18,15 @@ public class HitBox : MonoBehaviour
         {
             other.rigidbody.AddForce(transform.up * force);
             other.rigidbody.useGravity = true;
+        }
+    }
+
+    void TriggerDestroy()
+    {
+        timeSpentActive -= Time.deltaTime;
+        if (timeSpentActive <= 0)
+        {
+            Destroy(gameObject);
         }
     }
 }
